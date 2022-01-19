126fa4f26fb740f4e0b66062d26bfcc91595389a
dsamyliak <dsamyliak@gmail.com>
2022-01-20 01:31:15 +0200

    add section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
+import React from 'react';
+import Section from "./components/Section/Section";
 
-function App() {
+
+class App extends React.Component {
+  
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+
+
+  render() {
+
   return (
     <div className="App">
-      <h1>Phonebook Task 2 of HW-2</h1>
+      <><h1>Phonebook Task 2 of HW-2</h1></>
+      <Section title="Phonebook" numberH="1">
+        <form>
+          <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+        </form>
+      </Section>
+      <Section title="Contacts" numberH="2"></Section>
     </div>
-  );
-}
+    )
+  };
+};
 
 export default App;
